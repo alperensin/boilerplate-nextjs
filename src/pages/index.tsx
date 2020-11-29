b465cf1278f5fdf53649881dda20c01d81302eb5
alperensin <andreluizperensin@hotmail.com>
2020-11-29 19:04:12 -0300

    Add RTL config and first test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,21 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../../styles/Home.module.css';
 
-export default function Home() {
+type Props = {
+  title: string;
+};
+
+export default function Home({ title = 'Create Next App!' }: Props) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>{title}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          React <a href="https://nextjs.org">Avançado!</a>
         </h1>
 
         <p className={styles.description}>
@@ -61,5 +65,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
