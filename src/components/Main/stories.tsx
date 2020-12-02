19a1c2f0411431574492860e697af53a7187cc0f
alperensin <andre.silva@gelt.com.br>
2020-12-01 21:52:04 -0300

    Change knob modules from StoryBook to Essentials (use control from StoryBook)

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,19 +1,19 @@
 import { Story, Meta } from '@storybook/react/types-6-0';
-import { withKnobs, text } from '@storybook/addon-knobs';
 import Main from '.';
 
 export default {
   title: 'Main',
   component: Main,
-  decorators: [withKnobs]
+  args: {
+    title: 'Title Default',
+    description: 'Description Default'
+  }
 } as Meta;
 
-export const Basic: Story = () => (
-  <Main
-    title={text('Title', 'React Avançado')}
-    description={text(
-      'Description',
-      'TypeScript, ReactJS, NextJS e StyledComponents'
-    )}
-  />
-);
+export const Basic: Story = (args) => <Main {...args} />;
+Basic.args = {
+  title: 'Title Basic',
+  description: 'Description Basic Component'
+};
+
+export const Default: Story = (args) => <Main {...args} />;
