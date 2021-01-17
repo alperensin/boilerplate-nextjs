3884d0d3dc983103fa739a5e561971883b783be7
alperensin <andreluizperensin@hotmail.com>
2021-01-17 19:22:53 -0300

    Fix config jest-styled-components / Create theme file / Setup ThemeProvider with declaration file / Config absolute path (Storybook setup)

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle, css } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
 
@@ -6,19 +6,24 @@ const GlobalStyles = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-  }
-
-  html {
-    font-size: 62.5%;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
 
   html, body, #__next {
     height: 100%;
   }
 
-  body {
-    font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
+  ${({ theme }) => css`
+    html {
+      font-size: 62.5%;
+    }
+    body {
+      font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+        Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+      font-size: ${theme.font.sizes.medium};
+    }
+  `}
 
 `;
 
